3a0a9f21cc7a13d7f45a0dd2ca9390b23c3c2342
Brennan Caldwell <brennan.caldwell@gmail.com>
2020-09-12 11:18:01 -0700

    html hello world

--- server/index.js
+++ server/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const port = 3000;
+
+app.use(express.static('./public/dist'));
 
 app.get('/', (req, res) => {
   res.send('Hello World');
